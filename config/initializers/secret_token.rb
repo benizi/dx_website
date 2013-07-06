6f4124232ee47fb07d76a73b800c2e2d19626184
Alex Vallejo <vallejo.alex@gmail.com>
2013-07-06 16:56:24 -0400

    Run rails new, configure gem file for rsped and make production, test and dev groups

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DxWebsite::Application.config.secret_key_base = '37563b695a8c9931ae6a98e62e3daf6f3883e33200648f8421a89e03be0ad03a9032c00a3cb16e07d4fadbccf2274f1cbcb5755b421e8e87f66d5da984115c07'
