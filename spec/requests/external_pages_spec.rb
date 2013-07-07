317962292da7373b096592831d0ad7bf640929af
Alex Vallejo <vallejo.alex@gmail.com>
2013-07-06 21:58:24 -0400

    Add missing end to external_pages integration test

--- spec/requests/external_pages_spec.rb
+++ spec/requests/external_pages_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 
-describe "ExternalPages" do
-  
-	describe "About page"
-		it "should have the content 'About'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('About')
-	end
-end
+describe "External pages" do
+
+  describe "About page" do
+
+    it "should have the content 'About'" do
+      visit '/external_pages/About'
+      expect(page).to have_content('About')
+    end
+  end
+
+    describe "About page" do
+
+    it "should have the content 'About'" do
+      visit '/external_pages/About'
+      expect(page).to have_content('About')
+    end#it should have...
+  end#describe About page
+
+end #describe external pages
