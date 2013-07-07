99720b27cd7b2ae87c130ebb6e71a538132e0391
Alex Vallejo <vallejo.alex@gmail.com>
2013-07-07 10:30:58 -0400

    Add title tests to external_pages_spec and add titles to external pages

--- spec/requests/external_pages_spec.rb
+++ spec/requests/external_pages_spec.rb
@@ -4,49 +4,49 @@ describe "External pages" do
 
 	describe "Index page" do
 
-    	it "should have the content 'Index'" do
+    	it "should have the title 'Pitt Delta Chi - Welcome'" do
       		visit '/'
-      		expect(page).to have_content('Index')
+      		expect(page).to have_title('Pitt Delta Chi - Welcome')
     	end
   	end
 
   	describe "About page" do
 
-    	it "should have the content 'About'" do
+    	it "should have the title 'Pitt Delta Chi - About'" do
       		visit '/about'
-      		expect(page).to have_content('About')
+      		expect(page).to have_title('Pitt Delta Chi - About')
     	end
   	end
 
     describe "Alumni page" do
 
-    	it "should have the content 'Alumni'" do
+    	it "should have the title 'Pitt Delta Chi - Alumni'" do
       		visit '/alumni'
-      		expect(page).to have_content('Alumni')
+      		expect(page).to have_title('Pitt Delta Chi - Alumni')
     	end#it should have...
   	end#describe Alumni page
 
   	describe "Brothers page" do
 
-    	it "should have the content 'Brothers'" do
+    	it "should have the title 'Pitt Delta Chi - Brothers'" do
       		visit '/brothers'
-      		expect(page).to have_content('Brothers')
+      		expect(page).to have_title('Pitt Delta Chi - Brothers')
     	end#it should have...
   	end#describe Brothers page
 
   	describe "Contact page" do
 
-    	it "should have the content 'Contact'" do
+    	it "should have the title 'Pitt Delta Chi - Contact'" do
       		visit '/contact'
-      		expect(page).to have_content('Contact')
+      		expect(page).to have_title('Pitt Delta Chi - Contact')
     	end#it should have...
   	end#describe contact page
 
   	describe "Rush page" do
 
-    	it "should have the content 'Rush'" do
+    	it "should have the title 'Pitt Delta Chi - Rush'" do
       		visit '/rush'
-      		expect(page).to have_content('Rush')
+      		expect(page).to have_title('Pitt Delta Chi - Rush')
     	end#it should have...
   	end#describe Rush page
 
