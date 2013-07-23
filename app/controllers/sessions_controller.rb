97f99ee07a2780852983b864534991724de06d5e
Alex Vallejo <vallejo.alex@gmail.com>
2013-07-22 20:20:32 -0400

    Handles signin failure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		member = Member.find_by(email: params[:session][:email])
+		if member && Member.authenticate(params[:session][:password])
+			#Sign in member
+		else
+			flash.now[:error] = "Invalid email/password"
+			render 'new'
+		end
 	end
 
 	def destroy
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -3,10 +3,33 @@ require 'spec_helper'
 describe "Authentication" do
 	subject { page }
 
-	describe "signin page" do
-		before { visit signin_path }
+  	describe "signin" do
+  		before { visit signin_path }
 
-		it { should have_content('Sign in') }
-		it { should have_title('Pitt Delta Chi - Sign in') }
-  end
+  		describe "with invalid information" do
+  			before { click_button "Sign in"}
+
+  			it { should have_title('Sign in') }
+  			it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+        describe "after visiting another page" do
+          before {click_link "Home"}
+          it {should_not have_selector('div.alert.alert-error')}
+        end
+  		end
+
+  		describe "with valid information" do
+  			let(:member) { FactoryGirl.create(:member) }
+  			before do
+  				fill_in "Email", with: member.email.upcase
+  				fill_in "Password", with: member.password
+  				click_button "Sign in"
+  			end
+
+  			it { should have_title(member.name) }
+  			it { should have_link('Profile', href: member_path(member)) }
+  			it { should have_link('Sign out', href: signout_path) }
+  			it { should_not have_link('Sign in', href: signin_path) }
+  		end
+  	end
 end
