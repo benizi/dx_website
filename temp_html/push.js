0c8e07a54b1119e1ee73a99de718c0e0e7e2937e
Benjamin R. Haskell <bhaskell@4moms.com>
2013-07-24 13:41:45 -0400

    Fixes the double-firing

--- temp_html/push.js
+++ temp_html/push.js
@@ -1,23 +1,22 @@
-pushright.onclick = function() {
-    $('#wrapper').toggleClass('side-nav-push-toleft');
-    $('.side-nav').toggleClass('side-nav-open');
-
-    /*if($('#wrapper').hasClass('side-nav-push-toleft')) {
+pushright.onclick = function(e) {
+    if($('#wrapper').hasClass('side-nav-push-toleft')) {
       $('#wrapper').removeClass('side-nav-push-toleft');
       $('.side-nav').removeClass('side-nav-open');
     }
     else {
       $('#wrapper').addClass('side-nav-push-toleft');
       $('.side-nav').addClass('side-nav-open');
-    }*/
+    }
+e.stopPropagation();
   };
 
-/*content.onclick = function() {
+content.onclick = function(e) {
     $('#wrapper').toggleClass('side-nav-push-toleft');
     $('.side-nav').toggleClass('side-nav-open');
     
-    /*if($('#wrapper').hasClass('side-nav-push-toleft')) {
+    if($('#wrapper').hasClass('side-nav-push-toleft')) {
       $('#wrapper').removeClass('side-nav-push-toleft');
       $('.side-nav').removeClass('side-nav-open');
     }
-  };*/
+e.stopPropagation();
+  };
