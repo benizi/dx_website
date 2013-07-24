7c94fae3d47cf3292c397c4d02041204a8d41649
Alex Vallejo <vallejo.alex@gmail.com>
2013-07-24 13:02:01 -0400

    Fixes the menu opening in firefox at mobile size

--- temp_html/push.js
+++ temp_html/push.js
@@ -1,18 +1,23 @@
-$(document).ready(function() {
-  pushright.onclick = function() {
-    if($('#wrapper').hasClass('side-nav-push-toleft')) {
+pushright.onclick = function() {
+    $('#wrapper').toggleClass('side-nav-push-toleft');
+    $('.side-nav').toggleClass('side-nav-open');
+
+    /*if($('#wrapper').hasClass('side-nav-push-toleft')) {
       $('#wrapper').removeClass('side-nav-push-toleft');
       $('.side-nav').removeClass('side-nav-open');
     }
     else {
       $('#wrapper').addClass('side-nav-push-toleft');
       $('.side-nav').addClass('side-nav-open');
-    }
+    }*/
   };
-  content.onclick = function() {
-    if($('#wrapper').hasClass('side-nav-push-toleft')) {
+
+/*content.onclick = function() {
+    $('#wrapper').toggleClass('side-nav-push-toleft');
+    $('.side-nav').toggleClass('side-nav-open');
+    
+    /*if($('#wrapper').hasClass('side-nav-push-toleft')) {
       $('#wrapper').removeClass('side-nav-push-toleft');
       $('.side-nav').removeClass('side-nav-open');
     }
-  };
-});
+  };*/
